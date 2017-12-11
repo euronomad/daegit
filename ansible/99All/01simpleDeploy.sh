2246babd93c3158085d698e29c7507890519ee43
Kim <euronomad@gmail.com>
2017-12-11 15:30:53 +0900

    test

--- ansible/99All/01simpleDeploy.sh
+++ ansible/99All/01simpleDeploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 cd ../02vmWithARM
-/bin/bash 00.azuredeploy.sh -g daekimVMRG01 -n vmWithAnsible -u daekim -r minschoVaultRG01 -v daekimVault -s daekimPub -c dummy-init.yaml
+/bin/bash 00.azuredeploy.sh -g daekimVMRG01 -n vmWithAnsible -u daekim -r daekimVaultRG01 -v daekimVault01 -s daekimPub -c dummy-init.yaml
 cd ../04CommonTasks
 /usr/local/bin/ansible-playbook common_tasks.yml
 
