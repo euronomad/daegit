b8d59f01dee67abcbb2ea7cb2350f673e7a93349
Kim <euronomad@gmail.com>
2017-12-29 13:18:10 +0900

    t

--- mongodbgit/01Installation/azTemplates/03.setupTestEnv.sh
+++ mongodbgit/01Installation/azTemplates/03.setupTestEnv.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-az group create --name minschoMongoDBRG01 --location koreacentral
-az group deployment create --name networkForMongoDBDeployment --resource-group minschoNetRG \
+az group create --name daekimMongoDBRG01 --location koreacentral
+az group deployment create --name networkForMongoDBDeployment --resource-group daekimNETRG01 \
 --template-file 00.mongoNet.json \
---parameters vnet_name=minschoVnet01 \
+--parameters vnet_name=daekimVnet01 \
 --parameters subnet_name=testSubnet01 \
 --parameters publicip_name=mongoDBSingleNodeIP \
---parameters dnsPrefix=mongodbtest01 \
+--parameters dnsPrefix=mongodk01 \
 --parameters nic_name=mongodbTestNic01
 
 
-az group deployment create --name createVM --resource-group minschoMongoDBRG01 --template-file 02.vm.json \
+az group deployment create --name createVM --resource-group daekimMongoDBRG01 --template-file 02.vm.json \
 --parameters vm_name=mongoTestVM01 \
---parameters adminUserId=minsoojo \
+--parameters adminUserId=daekim \
 --parameters nic_name=mongodbTestNic01
 
 # disk 두 개 만듬
-az group deployment create --name createDataDisk --resource-group minschoMongoDBRG01 --template-file 01.dataDisk.json \
+az group deployment create --name createDataDisk --resource-group daekimMongoDBRG01 --template-file 01.dataDisk.json \
 --parameters dataDisk_name=mongodbDataDisk01
-az group deployment create --name createDataDisk --resource-group minschoMongoDBRG01 --template-file 01.dataDisk.json \
+az group deployment create --name createDataDisk --resource-group daekimMongoDBRG01 --template-file 01.dataDisk.json \
 --parameters dataDisk_name=mongodbDataDisk02
 # data disk attach
-az vm disk attach --vm-name mongoTestVM01 --disk mongodbDataDisk01 --lun 0 --resource-group minschoMongoDBRG01
-az vm disk attach --vm-name mongoTestVM01 --disk mongodbDataDisk02 --lun 1 --resource-group minschoMongoDBRG01
+az vm disk attach --vm-name mongoTestVM01 --disk mongodbDataDisk01 --lun 0 --resource-group daekimMongoDBRG01
+az vm disk attach --vm-name mongoTestVM01 --disk mongodbDataDisk02 --lun 1 --resource-group daekimMongoDBRG01
 
 #disk lun 확인
-az vm show --resource-group minschoMongoDBRG01 --name mongoTestVM01 | jq -r .storageProfile.dataDisks
-az vm show --resource-group minschoMongoDBRG01 --name mongoTestVM01 | jq -r .storageProfile.dataDisks[].lun
+az vm show --resource-group daekimMongoDBRG01 --name mongoTestVM01 | jq -r .storageProfile.dataDisks
+az vm show --resource-group daekimMongoDBRG01 --name mongoTestVM01 | jq -r .storageProfile.dataDisks[].lun
